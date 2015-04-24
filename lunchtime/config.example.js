dd8d3e3b69f90f3ad4f2244974be8bc480b6446b
Jamie Penney <jamie@jamiepenney.co.nz>
2015-04-24 18:57:44 +1200

    Add example config.js

---
+++ lunchtime/config.example.js
@@ -0,0 +1,19 @@
+﻿module.exports = {
+  round: '1',
+  slackUrl: 'https://hooks.slack.com/services/THISISNOTAREALSLACKWEBHOOKURL',
+  users: [
+    { user: 'Alex', token: 'AAAA' },
+    { user: 'Jamie', token: 'BBBB' },
+  ],
+  choices: [
+    { id: 1, name: 'Rogue and Vagabond - Pizza' },
+    { id: 2, name: 'Southern Cross' },
+    { id: 3, name: 'Burger Fuel' },
+    { id: 4, name: 'R & S Satay House' },
+    { id: 5, name: 'Ekim Burger' },
+    { id: 6, name: 'Sushi Train' },
+    { id: 7, name: 'Tulsi - Curry' },
+    { id: 8, name: 'Aroy - Thai' },
+    { id: 9, name: 'The Oaks Noodle House' }
+  ]
+};
