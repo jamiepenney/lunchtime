d31143195092ae54882dc51a612dbb5deef25f1d
Jamie Penney <jamiepenney@users.noreply.github.com>
2015-05-22 10:59:00 +1200

    Add Little Penang to the choices list

--- lunchtime/choices.js
+++ lunchtime/choices.js
@@ -8,6 +8,7 @@
     { id: 6, name: 'Sushi Train' },
     { id: 7, name: 'Tulsi - Curry' },
     { id: 8, name: 'Aroy - Thai' },
-    { id: 9, name: 'The Oaks Noodle House' }
+    { id: 9, name: 'The Oaks Noodle House' },
+    { id: 10, name: 'Little Penang', addedIn: 5 },
   ]
 }
