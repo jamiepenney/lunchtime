bdbec9641ce95893eba00b589f20e071e962548a
Jamie Penney <jamie@jamiepenney.co.nz>
2015-09-24 19:54:23 +1200

    Fix up the votes db query so that it gets the winner correctly

--- src/database/getChoicesForRound.sql
+++ src/database/getChoicesForRound.sql
@@ -1,10 +1,7 @@
-select c.id, c.name, count(v.id) as votes from "choice" c
-left outer join
-(  select v.* from vote v
-	join "round" r on v.round_id = r.id
-	where r.id = $1
-) as v on v.choice_id = c.id
+select c.id, c.name, r.winning_choice_id = c.id as winner, count(v.id) as votes from choice c
+join round r on r.id = $1
+left outer join vote v on v.choice_id = c.id and v.round_id = $1
 where (c.added_in is null or c.added_in <= $1)
 and (c.removed_in is null or c.removed_in >= $1)
-group by c.id
+group by c.id, r.winning_choice_id
 order by c.id asc;
