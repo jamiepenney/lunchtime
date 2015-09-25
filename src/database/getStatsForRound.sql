e82cd9b6d8878f4a53042749b1a9fe1558d0f10d
Jamie Penney <jamiepenney@users.noreply.github.com>
2015-09-25 13:16:47 +1200

    Show winning vote in green

--- src/database/getStatsForRound.sql
+++ src/database/getStatsForRound.sql
@@ -1,5 +1,6 @@
 select u.id, u.name, c.name as choice,
-case when r.winning_choice_id = c.id then true else false end as winner
+case when r.winning_choice_id = c.id then true else false end as winner,
+case when r.winning_vote_id = v.id then true else false end as vote_winner
 from vote v join "user" u on v.user_id = u.id
 join choice c on v.choice_id = c.id
 join round r on v.round_id = r.id
