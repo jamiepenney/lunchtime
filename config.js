aa86cc22dc8b5f4078dc9bfa93b1bed72d501af0
Jamie Penney <jamiepenney@users.noreply.github.com>
2015-10-20 13:59:38 +1300

    Pull config info from environment variables

---
+++ config.js
@@ -0,0 +1,8 @@
+﻿var config = {
+  slackUrl: process.env.SLACK_URL || 'https://hooks.slack.com/services/THISISNOTAREALSLACKWEBHOOKURL',
+  slackRoom: process.env.SLACK_ROOM || '#testing',
+  raygunApiKey: process.env.RAYGUN_API_KEY || '',
+  pg: process.env.DATABASE_URL || 'postgres://lunchtime:Password1@127.0.0.1:5432/lunchtime'
+};
+
+module.exports = config;
